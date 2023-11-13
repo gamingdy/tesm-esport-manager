ee33f6fd4f438b66f7701b91add7f02e38d1838b
Alex <alexpelletier2003@gmail.com>
2023-11-13 10:05:57 +0100

    feat: ajout equals et hashcode

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -1,5 +1,7 @@
 package modele;
 
+import java.util.Objects;
+
 public class Joueur {
 	
 	private int id;
@@ -28,6 +30,21 @@ public class Joueur {
 	}
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nom, pseudo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Joueur other = (Joueur) obj;
+		return id == other.id && Objects.equals(nom, other.nom) && Objects.equals(pseudo, other.pseudo);
 	}
 
 }
