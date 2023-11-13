ba2d913ca35083f9b18333dd115799d9b7371423
Sofia Gribanova <grisoffa@gmail.com>
2023-11-13 11:54:40 +0100

    feat: ajout exception custom PointsNegatifs

---
+++ src/exceptions/PointsNegatifs.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class PointsNegatifs extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public PointsNegatifs(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
