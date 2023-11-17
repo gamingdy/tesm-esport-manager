f440becab44ad65a228ff939c9f16b6d5902cd42
Gamingdy <christgik12@gmail.com>
2023-11-17 16:31:06 +0100

    feat: changement changement des boutons reduire et fermer, et ajout du logo

--- src/vue/main/TitleBar.java
+++ src/vue/main/TitleBar.java
@@ -7,31 +7,39 @@ import java.awt.event.*;
 public class TitleBar extends JPanel {
 
 	private Point coordsWin;
+	final ButtonTitleBar minimize;
+	final ButtonTitleBar exit;
 
 	public TitleBar(JFrame mainWindow) {
-		super((LayoutManager) new FlowLayout(FlowLayout.RIGHT));
+		super(new BorderLayout());
 
 		mainWindow.setUndecorated(true);
+		Color titleBarColor = new Color(25, 11, 52);
 
-		final JButton minimize = new JButton("-");
-		final JButton exit = new JButton("X");
+		minimize = new ButtonTitleBar("Reduire", titleBarColor);
+		exit = new ButtonTitleBar("Fermer", titleBarColor);
 
-		exit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		minimize.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				mainWindow.setState(JFrame.ICONIFIED);
-			}
-		});
+		exit.addActionListener(e -> System.exit(0));
+		exit.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 
+		minimize.addActionListener(e -> mainWindow.setState(JFrame.ICONIFIED));
+		minimize.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 
-		this.add(minimize);
-		Color color = new Color(25, 11, 52);
-		this.setBackground(color);
-		this.add(exit);
+		JPanel titleBarButton = new JPanel(new GridLayout(1, 2));
+		titleBarButton.setBackground(titleBarColor);
+		titleBarButton.add(minimize);
+		titleBarButton.add(exit);
+
+		JPanel titleBarIcon = new JPanel(new BorderLayout());
+		titleBarIcon.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+		titleBarIcon.setBackground(titleBarColor);
+		ImageIcon logo = new ImageIcon("assets/logo.png");
+		Image newimg = logo.getImage().getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
+		titleBarIcon.add(new JLabel(new ImageIcon(newimg)), BorderLayout.CENTER);
+
+		this.setBackground(titleBarColor);
+		this.add(titleBarButton, BorderLayout.EAST);
+		this.add(titleBarIcon, BorderLayout.WEST);
 		this.setBorder(BorderFactory.createMatteBorder(0, 0, 4, 0, Color.black));
 
 
@@ -45,7 +53,6 @@ public class TitleBar extends JPanel {
 				coordsWin = e.getPoint();
 			}
 		});
-
 
 		addMouseMotionListener(new MouseAdapter() {
 
