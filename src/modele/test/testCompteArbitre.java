296338628c958fc513938b68900936c69bdcf9a9
Sofia Gribanova <grisoffa@gmail.com>
2023-11-17 16:55:05 +0100

    feat : test compte arbitre compte Utilisateur

---
+++ src/modele/test/testCompteArbitre.java
@@ -0,0 +1,52 @@
+package modele.test;
+
+import exceptions.ErreurDate;
+import exceptions.FausseDate;
+import modele.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testCompteArbitre {
+	private CompteArbitre compteArbitre;
+	private Arbitre a;
+	private Tournoi tournoi;
+	private Saison s;
+
+	@Before
+	public void setUp() throws Exception {
+		a = new Arbitre(1, "Michel", "Jean");
+		CustomDate d1 = new CustomDate(2022, 11, 13);
+		CustomDate d2 = new CustomDate(2022, 10, 10);
+		s = new Saison(2022);
+
+		tournoi = new Tournoi(s, "RLCS", d1, d2, Niveau.INTERNATIONAL);
+		compteArbitre = new CompteArbitre("arbitre1", "123", a, tournoi);
+	}
+
+	@Test
+	public void getArbitre() {
+		assertEquals(a, compteArbitre.getArbitre());
+	}
+
+	@Test
+	public void setArbitre() {
+		Arbitre b = new Arbitre(2, "Christian", "G");
+		compteArbitre.setArbitre(b);
+		assertEquals(b, compteArbitre.getArbitre());
+
+	}
+
+	@Test
+	public void getTournoi() {
+		assertEquals(tournoi, compteArbitre.getTournoi());
+	}
+
+	@Test
+	public void setTournoi() throws ErreurDate, FausseDate {
+		Tournoi t1 = new Tournoi(s, "RLCS23", new CustomDate(2022, 10, 2), new CustomDate(2022, 11, 3), Niveau.INTERNATIONAL);
+		compteArbitre.setTournoi(t1);
+		assertEquals(t1, compteArbitre.getTournoi());
+	}
+}
---
+++ src/modele/test/testCompteUtilisateur.java
@@ -0,0 +1,26 @@
+package modele.test;
+
+import modele.CompteUtilisateur;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testCompteUtilisateur {
+	private CompteUtilisateur compte;
+
+	@Before
+	public void setUp() throws Exception {
+		compte = new CompteUtilisateur("admin", "1234");
+	}
+
+	@Test
+	public void getUsername() {
+		assertEquals("admin", compte.getUsername());
+	}
+
+	@Test
+	public void getMdp() {
+		assertEquals("1234", compte.getMdp());
+	}
+}
