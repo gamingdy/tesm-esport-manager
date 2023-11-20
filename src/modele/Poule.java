af0733bf3d5a7c1abea4071bb3b95d5f841c5faf
username-Everam <simsimi.armand@gmail.com>
2023-11-20 15:44:32 +0100

    Fix : Delete ce qu'il y avait en trop
    Axel avait raison on a fait trop de liste

--- src/modele/Poule.java
+++ src/modele/Poule.java
@@ -1,22 +1,13 @@
 package modele;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import exceptions.PouleComplete;
-
 public class Poule {
 
 	private Tournoi tournoi;
-
 	private Character libelle;
-	private Map<Equipe,Integer> equipes;
 	
 	public Poule(Tournoi tournoi, Character libelle) {
 		this.tournoi = tournoi;
 		this.libelle = libelle;
-		this.equipes = new HashMap<Equipe,Integer>();
 	}
 
 	public Tournoi getTournoi() {
@@ -28,42 +19,5 @@ public class Poule {
 
 	public void setLibelle(Character libelle) {
 		this.libelle = libelle;
-	}
-
-	public Integer getPoint(Equipe equipe){
-		return this.equipes.get(equipe);
-	}
-
-	public void ajouterPoint(Equipe equipe, Integer point){
-		this.equipes.put(equipe, this.equipes.get(equipe)+point);
-	}
-
-	public void enleverPoint(Equipe equipe, Integer point){
-		this.equipes.put(equipe, this.equipes.get(equipe) - point);
-	}
-	
-	public void addEquipe(Equipe equipe) throws PouleComplete {
-		if (this.equipes.size() == 8) {
-			throw new PouleComplete("La poule est complète");
-		}
-		this.equipes.put(equipe,0);
-	}
-
-	public void deleteEquipe(Equipe equipe) {
-		this.equipes.remove(equipe);
-	}
-
-	public Set<Equipe> getEquipes() {
-		return this.equipes.keySet();
-	}
-
-	@Override
-	public String toString(){
-		String str = "";
-		for (Equipe e : this.getEquipes()){
-			str += e.getNom() + " : " + this.equipes.get(e);
-			str += System.lineSeparator();
-		}
-		return str;
 	}
 }
