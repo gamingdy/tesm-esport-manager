5a15cfd360edc5e4a062a86088172d809506a8b2
Sofia Gribanova <grisoffa@gmail.com>
2023-11-20 12:27:58 +0100

    fix : supp un constructeur matche

--- src/modele/test/testEquipe.java
+++ src/modele/test/testEquipe.java
@@ -18,39 +18,32 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class testEquipe {
-	Equipe equipe1;
-	Joueur j;
-	Equipe equipe2;
-	Joueur j2 = new Joueur("Soso1", "Faze");
-	Joueur j3 = new Joueur("SoSo2", "Faze");
-	Joueur j4 = new Joueur("Sososo3", "Faze");
-	Joueur j5 = new Joueur("Sosososo4", "Faze");
-	Joueur j6 = new Joueur("Simon", "Faze");
+	private Equipe equipe1;
+	private Joueur j;
+	private Equipe equipe2;
+	private Joueur j2;
+	private Joueur j3;
+	private Joueur j4;
+	private Joueur j5;
+	private Joueur j6;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws EquipeComplete {
 		equipe1 = new Equipe("Faze", Country.ALGERIA);
-		j = new Joueur("Cricri", "Faze");
+
+		j2 = new Joueur("Soso1", equipe1);
+		j3 = new Joueur("SoSo2", equipe1);
+		j4 = new Joueur("Sososo3", equipe1);
+		j5 = new Joueur("Sosososo4", equipe1);
 		equipe2 = new Equipe("Patate", Country.ALGERIA);
 	}
 
-	@After
-	public void tearDown() {
-		equipe1 = null;
-		equipe2 = null;
-	}
-
-	@Test
-	public void testAjoutJoueur() throws EquipeComplete {
-		equipe1.addJoueur(j);
-		assertEquals(1, equipe1.getNombreJoueurs());
-	}
 
 	@Test
 	public void testAjoutMemeJoueur() throws EquipeComplete {
-		equipe1.addJoueur(j);
-		equipe1.addJoueur(j);
-		assertEquals(1, equipe1.getNombreJoueurs());
+		j = new Joueur("Cricri", equipe1);
+		j = new Joueur("Cricri", equipe1);
+		assertEquals(4, equipe1.getNombreJoueurs());
 	}
 
 	@Test
@@ -69,60 +62,37 @@ public class testEquipe {
 		assertEquals("Palau", Country.PALAU.getCountry());
 	}
 
-	@Test
-	public void testAjoutJoueurDifferent() throws EquipeComplete {
-		Joueur j2 = new Joueur("Soso", "Faze");
-		equipe1.addJoueur(j);
-		equipe1.addJoueur(j2);
-		assertEquals(2, equipe1.getNombreJoueurs());
-	}
 
 	@Test
 	public void testAjout5joueurs() throws EquipeComplete {
-		equipe1.addJoueur(j);
-		equipe1.addJoueur(j2);
-		equipe1.addJoueur(j3);
-		equipe1.addJoueur(j4);
-		equipe1.addJoueur(j5);
+		j = new Joueur("Cricri", equipe1);
 		assertEquals(5, equipe1.getNombreJoueurs());
 	}
 
 	@Test(expected = EquipeComplete.class)
 	public void testAjout6joueursException() throws EquipeComplete {
-		equipe1.addJoueur(j);
-		equipe1.addJoueur(j2);
-		equipe1.addJoueur(j3);
-		equipe1.addJoueur(j4);
-		equipe1.addJoueur(j5);
-		equipe1.addJoueur(j6);
-	}
-
-	@Test
-	public void testNombreJoueursEquipe() {
-		assertEquals(0, equipe1.getNombreJoueurs());
+		j6 = new Joueur("exception", equipe1);
 	}
 
 	@Test(expected = JoueurNonPresent.class)
 	public void testgetJoueurInexistant() throws JoueurNonPresent, EquipeVide, EquipeComplete {
-		equipe1.addJoueur(j);
-		equipe1.getJoueur(new Joueur("klklk", "Faze"));
+		equipe1.getJoueur(new Joueur("klklk", equipe2));
 	}
 
 	@Test(expected = EquipeVide.class)
 	public void testgetJoueurEquipeVide() throws JoueurNonPresent, EquipeVide, EquipeComplete {
-		equipe1.getJoueur(j);
+		equipe1.getJoueur(j2);
 	}
 
 	@Test
 	public void testSupprimerJoueur() throws JoueurNonPresent, EquipeVide, EquipeComplete {
-		equipe1.addJoueur(j);
-		equipe1.deleteJoueur(j);
-		assertEquals(0, equipe1.getNombreJoueurs());
+		equipe1.deleteJoueur(j2);
+		assertEquals(3, equipe1.getNombreJoueurs());
 	}
 
 	@Test(expected = EquipeVide.class)
 	public void testSupprimerJoueurEquipeVide() throws JoueurNonPresent, EquipeVide {
-		equipe1.deleteJoueur(j);
+		equipe2.deleteJoueur(j);
 	}
 
 	@Test(expected = JoueurNonPresent.class)
@@ -133,8 +103,7 @@ public class testEquipe {
 
 	@Test
 	public void testgetJoueurNormal() throws JoueurNonPresent, EquipeVide, EquipeComplete {
-		equipe1.addJoueur(j);
-		assertEquals(j, equipe1.getJoueur(j));
+		assertEquals(j, equipe1.getJoueur(j2));
 	}
 
 	@Test
