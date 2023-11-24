d7a13ff9f03263d7097273cd555716edc7afad67
Sofia Gribanova <grisoffa@gmail.com>
2023-11-24 16:21:16 +0100

    fix: renommer les fichiers necessaires

---
+++ src/modele/test/TestPartie.java
@@ -0,0 +1,25 @@
+package modele.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestPartie {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void setNumeroPartie() {
+	}
+
+	@Test
+	public void setVainqueur() {
+	}
+
+	@Test
+	public void setMatche() {
+	}
+}
