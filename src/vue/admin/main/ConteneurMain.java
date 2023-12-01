3f620b9c81f7828c6e803fbd3b186dafd7b85f46
Titouan <titouan.brando@etu.iut-tlse3.fr>
2023-12-01 15:52:47 +0100

    Fix: plus les données de démo

--- src/vue/admin/main/ConteneurMain.java
+++ src/vue/admin/main/ConteneurMain.java
@@ -26,32 +26,7 @@ public class ConteneurMain extends JPanel {
 		
 		cardLayout = new CardLayout();
 		this.setLayout(cardLayout);
-		DefaultListModel<LigneEquipe> dl = new DefaultListModel<LigneEquipe>();
-		dl.addElement(new LigneEquipe(1, new ImageIcon("assets/logo.png"), "La première équipe", 14525));
-		dl.addElement(new LigneEquipe(2, new ImageIcon("assets/logo.png"), "La deuxième équipe", 1616));
-		dl.addElement(new LigneEquipe(3, new ImageIcon("assets/logo.png"), "La troisème équipe", 15));
-		DefaultListModel<LigneTournoi> lm = new DefaultListModel<LigneTournoi>();
-		lm.addElement(new LigneTournoi("Tournoi en cours actuellement",true));
-		lm.addElement(new LigneTournoi("Tournoi terminé récent",false));
-		lm.addElement(new LigneTournoi("Tournoi terminé moins récent",false));
-		lm.addElement(new LigneTournoi("Tournoi terminé encore moins récent",false));
-		lm.addElement(new LigneTournoi("Tournoi terminé vieux",false));
-		lm.addElement(new LigneTournoi("Tournoi terminé très vieux ",false));
-		lm.addElement(new LigneTournoi("Tournoi terminé vriament très vieux",false));
-		lm.addElement(new LigneTournoi("Premier tournoi de la saison",false));
-		DefaultListModel<LigneMatche> lt = new DefaultListModel<LigneMatche>();
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		lt.addElement(new LigneMatche("18/11/2023 18:30",new ImageIcon("assets/logo.png"),"Equipe1",new ImageIcon("assets/trophéePerdant.png"),new ImageIcon("assets/logo.png"),"Equipe2",new ImageIcon("assets/trophéeGagnant.png")));
-		vueAccueil = new VueAccueil(dl,lm,lt);
+		vueAccueil = new VueAccueil(new DefaultListModel<LigneEquipe>(),new DefaultListModel<LigneTournoi>(),new DefaultListModel<LigneMatche>());
 		add(vueAccueil, "Accueil");
 		show("Accueil");
 	}
