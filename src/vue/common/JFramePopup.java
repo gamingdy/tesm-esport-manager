d52538d558341313f656b20f980e6db940cd980d
Sofia Gribanova <grisoffa@gmail.com>
2023-12-04 16:23:34 +0100

    feat : customPopup

---
+++ src/vue/common/JFramePopup.java
@@ -0,0 +1,85 @@
+package vue.common;
+
+import controller.VueObserver;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+public class JFramePopup extends JFrame {
+
+	public JFramePopup(String title, String message) {
+		super(title);
+		setType(Type.UTILITY);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setPreferredSize(new Dimension(400, 200));
+
+
+		ImageIcon icon = new ImageIcon(("assets/logo.png"));
+		setIconImage(icon.getImage());
+
+		JPanel panel = createPanel(message);
+		add(panel);
+
+		pack();
+		setLocationRelativeTo(null); // Centre la fenêtre sur l'écran
+		setVisible(true);
+	}
+
+	private JPanel createPanel(String message) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBackground(new Color(15, 3, 25));
+
+		JLabel label = new JLabel(message);
+		label.setForeground(Color.white);
+		label.setHorizontalAlignment(SwingConstants.CENTER); // Centre le texte
+		label.setFont(MaFont.getFontTitre3());// Agrandir la police
+
+		panel.add(label, BorderLayout.CENTER);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.setBackground(new Color(15, 3, 25));
+		JButton closeButton = new JButton("Fermer");
+		closeButton.setFont(MaFont.getFontTitre4());
+		closeButton.setForeground(CustomColor.BLANC);
+		closeButton.setBackground(CustomColor.TRANSPARENT);
+		closeButton.setOpaque(false);
+		closeButton.setFocusable(false);
+		closeButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose(); // Ferme la fenêtre lorsque le bouton est cliqué
+			}
+		});
+		buttonPanel.add(closeButton);
+
+		JButton okButton = new JButton("OK");
+		okButton.setFont(MaFont.getFontTitre4());
+		okButton.setForeground(CustomColor.BLANC);
+		okButton.setBackground(CustomColor.TRANSPARENT);
+		okButton.setOpaque(false);
+		okButton.setFocusable(false);
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VueObserver.getInstance().notifyVue("Login");
+				dispose();
+			}
+		});
+		buttonPanel.add(okButton);
+
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+
+		return panel;
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			new JFramePopup("Titre de la fenêtre", "Message à afficher");
+		});
+	}
+}
