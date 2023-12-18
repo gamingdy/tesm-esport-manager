4666cd014bd75fa662a0d0fe04f3982bd1a78125
Titouan <titouan.brando@etu.iut-tlse3.fr>
2023-12-18 10:09:40 +0100

    Feat: Ajout de la popup pour le pseudo

---
+++ src/vue/admin/equipes/creation/PopupPseudo.java
@@ -0,0 +1,86 @@
+package vue.admin.equipes.creation;
+
+import javax.swing.*;
+
+import vue.common.CustomColor;
+import vue.common.MaFont;
+
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class PopupPseudo extends JFrame {
+
+	private JTextField tf;
+
+	public interface ActionHandler {
+		void handleAction();
+	}
+
+	public PopupPseudo(String title, String message, ActionHandler actionHandler) {
+		super(title);
+		setType(Type.UTILITY);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setPreferredSize(new Dimension(400, 200));
+
+		ImageIcon icon = new ImageIcon(("assets/logo.png"));
+		setIconImage(icon.getImage());
+
+		JPanel panel = createPanel(actionHandler);
+		add(panel);
+
+		pack();
+		setLocationRelativeTo(null); // Centre la fenêtre sur l'écran
+		setVisible(true);
+	}
+
+	public JPanel createPanel(ActionHandler actionHandler) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBackground(new Color(15, 3, 25));
+
+		JLabel label = new JLabel("Saisir le pseudo :");
+		label.setForeground(Color.white);
+		label.setHorizontalAlignment(SwingConstants.CENTER); // Centre le texte
+		label.setFont(MaFont.getFontTitre3()); // Agrandir la police
+		panel.add(label, BorderLayout.NORTH);
+		
+		tf = new JTextField(25);
+		tf.setForeground(CustomColor.BLANC);
+		tf.setBackground(CustomColor.BACKGROUND_MENU);
+		tf.setBorder(BorderFactory.createLineBorder(CustomColor.ROSE_CONTOURS,2));
+		tf.setFont(MaFont.getFontTitre4());
+		panel.add(tf, BorderLayout.CENTER);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.setBackground(new Color(15, 3, 25));
+		JButton closeButton = new JButton("Annuler");
+		closeButton.setFont(MaFont.getFontTitre4());
+		closeButton.setForeground(CustomColor.BLANC);
+		closeButton.setOpaque(false);
+		closeButton.setFocusable(false);
+		closeButton.addActionListener(e -> dispose());
+		buttonPanel.add(closeButton);
+
+		JButton okButton = new JButton("Valider");
+		okButton.setFont(MaFont.getFontTitre4());
+		okButton.setForeground(CustomColor.BLANC);
+		okButton.setOpaque(false);
+		okButton.setFocusable(false);
+		okButton.addActionListener(e -> {
+			if (actionHandler != null) {
+				actionHandler.handleAction();
+			}
+			dispose();
+		});
+		buttonPanel.add(okButton);
+
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+
+		return panel;
+	}
+	
+	public String getSaisie() {
+		return tf.getText();
+	}
+}
