e449d2ffbf18d9592974ea58e60fdc7ef7ea75ad
username-Everam <simsimi.armand@gmail.com>
2023-12-20 10:34:52 +0100

    Add 2 nouveaux popups

---
+++ src/vue/common/JFramePopupInfo.java
@@ -0,0 +1,67 @@
+package vue.common;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class JFramePopupInfo extends JFrame {
+
+	public interface ActionHandler {
+		void handleAction();
+	}
+
+	public JFramePopupInfo(String title, String message, ActionHandler actionHandler) {
+		super(title);
+		setType(Type.UTILITY);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setPreferredSize(new Dimension(400, 200));
+
+		ImageIcon icon = new ImageIcon(("assets/logo.png"));
+		setIconImage(icon.getImage());
+
+		JPanel panel = createPanel(message, actionHandler);
+		add(panel);
+
+		pack();
+		setLocationRelativeTo(null); // Centre la fenêtre sur l'écran
+		setVisible(true);
+	}
+
+	private JPanel createPanel(String message, ActionHandler actionHandler) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBackground(new Color(15, 3, 25));
+
+		JLabel label = new JLabel(message);
+		label.setForeground(Color.white);
+		label.setHorizontalAlignment(SwingConstants.CENTER); // Centre le texte
+		label.setFont(MaFont.getFontTitre3()); // Agrandir la police
+
+		panel.add(label, BorderLayout.CENTER);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.setBackground(new Color(15, 3, 25));
+
+		JButton closeButton = new JButton("OK");
+		closeButton.setFont(MaFont.getFontTitre4());
+		closeButton.setForeground(CustomColor.BLANC);
+		closeButton.setBackground(CustomColor.TRANSPARENT);
+		closeButton.setOpaque(false);
+		closeButton.setFocusable(false);
+		closeButton.addActionListener(e -> dispose());
+		buttonPanel.add(closeButton);
+
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+
+		return panel;
+	}
+}
