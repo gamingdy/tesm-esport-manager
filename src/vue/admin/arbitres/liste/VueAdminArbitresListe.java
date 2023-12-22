21ecbb3dec649b599261a0fafa419cf9f7da4d30
Titouan <titouan.brando@etu.iut-tlse3.fr>
2023-12-22 15:36:16 +0100

    Fix: liste des arbitres

--- src/vue/admin/arbitres/liste/VueAdminArbitresListe.java
+++ src/vue/admin/arbitres/liste/VueAdminArbitresListe.java
@@ -41,6 +41,12 @@ public class VueAdminArbitresListe extends JPanel {
 		list.setBackground(CustomColor.BACKGROUND_MAIN);
 		list.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 
+		JPanel j;
+		for (int i = 0; i<6;i++) {
+			j = new JPanel();
+			j.setOpaque(false);
+			list.add(j);
+		}
 
 		JScrollPane sp = new JScrollPane(list);
 		sp.setBackground(CustomColor.BACKGROUND_MAIN);
