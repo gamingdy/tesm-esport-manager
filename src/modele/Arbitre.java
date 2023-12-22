0fb0381cadc0e712bb4cc1cb3a776559d1381895
Gamingdy <christgik12@gmail.com>
2023-12-22 15:45:55 +0100

    fix:merge: ajout de Arbitre.java

--- src/modele/Arbitre.java
+++ src/modele/Arbitre.java
@@ -1,39 +1,41 @@
 package modele;
 
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Optional;
+
+import dao.Connexion;
+import dao.DaoArbitre;
 import exceptions.IdNotSetException;
 
 public class Arbitre implements Comparable<Arbitre> {
 	private Integer id;
 	private String nom;
 	private String prenom;
+	
+	public Arbitre (String nom,String prenom) throws SQLException {
+		this(nom,prenom,new DaoArbitre(Connexion.getConnexion()).getLastId());
+	}
 
-	public Arbitre(String nom, String prenom) {
+	private Arbitre(String nom, String prenom, Integer id) {
 		this.nom = nom;
 		this.prenom = prenom;
+		if (id==null) {
+			this.id=0;
+		} else {
+			this.id = id+1;
+		}
 	}
 
 	public String getNom() {
 		return nom;
 	}
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 
 	public String getPrenom() {
 		return prenom;
 	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-
-	@Override
-	public String toString() {
-		return this.getNom() + " " + this.getPrenom();
-	}
-
+	
 	@Override
 	public int compareTo(Arbitre autreArbitre) {
 		if (this == autreArbitre) {
@@ -43,13 +45,7 @@ public class Arbitre implements Comparable<Arbitre> {
 		}
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (o instanceof Arbitre) {
-			return this.toString().equals(o.toString());
-		}
-		return false;
-	}
+	
 
 	public Integer getId() throws IdNotSetException {
 		if (this.id == null) {
@@ -60,5 +56,35 @@ public class Arbitre implements Comparable<Arbitre> {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Arbitre [id=" + id + ", nom=" + nom + ", prenom=" + prenom + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nom, prenom);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Arbitre other = (Arbitre) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nom, other.nom) && Objects.equals(prenom, other.prenom);
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
 	}
 }
