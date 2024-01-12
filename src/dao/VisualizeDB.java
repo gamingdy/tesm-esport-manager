0260fa376431097217d57422e1c3d7db5742cdc6
Alex <alexpelletier2003@gmail.com>
2024-01-12 10:24:53 +0100

    fix : visualisation de la bd

--- src/dao/VisualizeDB.java
+++ src/dao/VisualizeDB.java
@@ -35,66 +35,78 @@ public class VisualizeDB {
 		DaoSelection daoselection = new DaoSelection(c);
 		DaoTournoi daotournoi = new DaoTournoi(c);
 		
-		System.out.println("_______________Appartenance_______________________");
+		/*System.out.println("_______________Appartenance_______________________");
 		System.out.println("\n");
 		List<Appartenance> l = daoappartenance.getAll();
 		l.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Arbitrage__________________________");
 		System.out.println("\n");
 		List<Arbitrage> l1 = daoarbitrage.getAll();
 		l1.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Arbitre____________________________");
 		System.out.println("\n");
 		List<Arbitre> l2 = daoarbitre.getAll();
 		l2.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		*/
 		System.out.println("_______________Equipe_____________________________");
 		System.out.println("\n");
 		List<Equipe> l3 = daoequipe.getAll();
 		l3.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Inscription________________________");
 		System.out.println("\n");
 		List<Inscription> l4 = daoinscription.getAll();
 		l4.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Joueur_____________________________");
 		System.out.println("\n");
 		List<Joueur> l5 = daojoueur.getAll();
 		l5.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Matche_____________________________");
 		System.out.println("\n");
 		List<Matche> l6 = daomatche.getAll();
 		l6.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Niveau_____________________________");
 		System.out.println("\n");
 		List<Niveau> l7 = daoniveau.getAll();
 		l7.stream().forEach(x -> System.out.println(x));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Partie_____________________________");
 		System.out.println("\n");
 		List<Partie> l8 = daopartie.getAll();
 		l8.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Poule______________________________");
 		System.out.println("\n");
 		List<Poule> l9 = daopoule.getAll();
 		l9.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Saison_____________________________");
 		System.out.println("\n");
 		List<Saison> l10 = daosaison.getAll();
 		l10.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Selection__________________________");
 		System.out.println("\n");
 		List<Selection> l11 = daoselection.getAll();
 		l11.stream().forEach(x -> System.out.println(x.toString()));
 		System.out.println("\n\n\n");
+		
 		System.out.println("_______________Tournoi____________________________");
 		System.out.println("\n");
 		List<Tournoi> l12 = daotournoi.getAll();
