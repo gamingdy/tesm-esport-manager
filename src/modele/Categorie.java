1162ecda13264dd78a6d139d901b4fca30586b18
Alex <alexpelletier2003@gmail.com>
2024-01-19 09:10:08 +0100

    feat : ajout des points selon la catégorie de match

--- src/modele/Categorie.java
+++ src/modele/Categorie.java
@@ -1,9 +1,31 @@
 package modele;
 
 public enum Categorie {
-	POULE,
-	FINALE,
-	DEMI_FINALE,
-	QUART_FINALE,
-	PETITE_FINALE,
+	POULE("POULE",25,15),
+	FINALE("FINALE",200,100),
+	DEMI_FINALE("DEMI_FINALE",0,0),
+	QUART_FINALE("QUART_FINALE",0,0),
+	PETITE_FINALE("PETIT_FINALE",50,25),;
+
+	private String designation;
+	private int ptsSaisonVictoire;
+	private int ptsSaisonDefaite;
+	
+	private Categorie(String designation, int ptsSaisonVictoire, int ptsSaisonDefaite) {
+		this.designation=designation;
+		this.ptsSaisonVictoire=ptsSaisonVictoire;
+		this.ptsSaisonDefaite=ptsSaisonDefaite;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public int getPtsSaisonVictoire() {
+		return ptsSaisonVictoire;
+	}
+
+	public int getPtsSaisonDefaite() {
+		return ptsSaisonDefaite;
+	}
 }
